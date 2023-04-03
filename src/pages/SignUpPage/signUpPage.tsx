6818532ed3956f59008607154a19b490234ec1f8
Luana Oliveiram <luanasilva.jua@gmail.com>
2023-04-03 17:46:59 -0300

    Implementando CSS module do signUpPage

--- src/pages/SignUpPage/signUpPage.tsx
+++ src/pages/SignUpPage/signUpPage.tsx
@@ -1,16 +1,16 @@
 import React from "react";
+import Style from './SignUpPage.module.css'
 import SignUpForm from "../../components/SignUpComponent/SignUpComponent";
 import registerImage from '../../assets/register_image.png';
-import './SignUpPage.css';
+import './SignUpPage.module.css';
 
 const SignUp = () => {
     return (
-        <div className="sign-up-page">
-            <div className="sign-up-page_container">
-                <img className="register-image" src={registerImage} alt="user register" />
+        <div className={Style.sign_up_page}>
+            <div className={Style.container} >
+                <img className={Style.register_image} src={registerImage} alt="user register" />
                 <SignUpForm />
             </div>
-            
         </div>
     );
 }
