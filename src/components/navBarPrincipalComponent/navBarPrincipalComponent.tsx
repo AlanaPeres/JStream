9cdcc18f1fddddda7a41fff8c64a923a9d9ef8b8
Lennan Renzo <lennancontato@gmail.com>
2023-04-03 17:13:02 -0300

    chore: navbar com ativação em relação a pagina feita

--- src/components/navBarPrincipalComponent/navBarPrincipalComponent.tsx
+++ src/components/navBarPrincipalComponent/navBarPrincipalComponent.tsx
@@ -1,29 +1,72 @@
-import { Link } from "react-router-dom";
+import { NavLink, NavLinkProps, useLocation } from "react-router-dom";
 import styles from "./navBarPrincipalComponent.module.css";
-import img from "../../assets/logo dark.png"
-import homebutton from "../../assets/home button.png"
-import dollarbutton from "../../assets/dollar button.png"
-import transferbutton from "../../assets/transfer button.png"
-import profileicon from "../../assets/profile icon.png"
+import img from "../../assets/logo dark.png";
+import homebutton from "../../assets/home button.png";
+import dollarbutton from "../../assets/dollar button.png";
+import transferbutton from "../../assets/transfer button.png";
+import profileicon from "../../assets/profile icon.png";
 
+interface NavButtonProps extends NavLinkProps {
+  activeClassName?: string;
+}
 
 export const NavBarPrincipal = () => {
+  const location = useLocation();
+
+  const navButtonProps: NavButtonProps = {
+    activeClassName: styles.active,
+    to: "/saldo",
+  };
+
+  const ExtratoBtnClassName =
+    location.pathname === "/extrato"
+      ? `${styles.btn} ${styles.extratobtnActive}`
+      : styles.btn;
+  const SaldoBtnClassName =
+    location.pathname === "/saldo"
+      ? `${styles.btn} ${styles.saldobtnActive}`
+      : styles.btn;
+  const Transferclassname =
+    location.pathname === "/transferencia"
+      ? `${styles.btn} ${styles.transferbtnActive}`
+      : styles.btn;
+
   return (
     <>
       <nav className={styles.navbar}>
-      <img className={styles.perfil} src={profileicon} alt="" />
+        <img className={styles.perfil} src={profileicon} alt="" />
         <ul className={styles.ul}>
           <li className={styles.li}>
-            <button className={styles.btn}><img className={styles.dollarbtn} src={dollarbutton} alt="" /></button>
+            <NavLink {...navButtonProps} to={"/extrato"}>
+              <button className={ExtratoBtnClassName}>
+                <img
+                  className={styles.imagemdollar}
+                  src={dollarbutton}
+                  alt=""
+                />
+              </button>
+            </NavLink>
           </li>
           <li className={styles.li}>
-            <button className={styles.btn}><img className={styles.homebtn} src={homebutton} alt="" /></button>
+            <NavLink {...navButtonProps} to={"/saldo"}>
+              <button className={SaldoBtnClassName}>
+                <img className={styles.imagemsaldo} src={homebutton} alt="" />
+              </button>
+            </NavLink>
           </li>
           <li className={styles.li}>
-            <button className={styles.btn}><img className={styles.transferbtn} src={transferbutton} alt="" /></button>
+            <NavLink {...navButtonProps} to={"/transferencia"}>
+              <button className={Transferclassname}>
+                <img
+                  className={styles.imagemtranfer}
+                  src={transferbutton}
+                  alt=""
+                />
+              </button>
+            </NavLink>
           </li>
         </ul>
-      <img className={styles.logotipo} src={img} alt=""/> 
+        <img className={styles.logotipo} src={img} alt="" />
       </nav>
     </>
   );
