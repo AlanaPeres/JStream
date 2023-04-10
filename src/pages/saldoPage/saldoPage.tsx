e55231ac5ea270cbea2c60c8a3255a718f28e26e
Luana Oliveiram <luanasilva.jua@gmail.com>
2023-04-10 14:14:06 -0300

    Implementando rotas da página de extrato

--- src/pages/saldoPage/saldoPage.tsx
+++ src/pages/saldoPage/saldoPage.tsx
@@ -1,7 +1,13 @@
 import { NavBarPrincipal } from "../../components/navBarPrincipalComponent/navBarPrincipalComponent";
 import { SaldoContent } from "../../components/saldoComponent/saldoComponent";
+import { UsersManager } from "../../service/usersManagers";
 
 export const SaldoPage = () => {
+  const userManager = new UsersManager();
+  if (!userManager.getSessionUser()) {
+    const host = window.location.host;
+    window.location.href = `http://${host}/login`;
+  }
   return (
     <> 
       <NavBarPrincipal/>
