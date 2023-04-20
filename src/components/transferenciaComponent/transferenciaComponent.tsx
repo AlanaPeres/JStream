71851cade54b65558597a901bd716914a06c40be
Lennan Renzo <lennancontato@gmail.com>
2023-04-20 18:39:54 -0300

    chore: ajustando css das paginas

--- src/components/transferenciaComponent/transferenciaComponent.tsx
+++ src/components/transferenciaComponent/transferenciaComponent.tsx
@@ -13,83 +13,81 @@ export const TransferenciaContent = () => {
   }
 
   return (
-    <div className={style.container}>
-      <div className={style.top}>
-        <div className={style.container_title_desktop}>
-          <h3 className={style.title_desktop}>Transferência</h3>
+    <div className={style.article}>
+          <div className={style.btnsair}>
+            <ButtonLougoutTextComponent
+              label="Sair"
+              onClick={handleClickLogoutButton}
+            />
+          </div>
+      <div className={style.container}>
+        <div className={style.top}>
+          <div className={style.container_title_desktop}>
+            <h3 className={style.title_desktop}>Transferência</h3>
+          </div>
         </div>
-        <ButtonLougoutTextComponent
-          label="Sair"
-          onClick={handleClickLogoutButton}
-        />
-      </div>
-      <div className={style.content}>
-        <h3 className={style.title_mobile}>Transferência</h3>
-        <form className={style.form}>
-          <div className={style.col_1}>
-            <div className={style.container_input_label}>
-              <input
-                type="number"
-                pattern="[0-9]{3}"
-                maxLength={3}
-                minLength={3}
-                required
-                autoComplete="off"
-                placeholder="Cod do banco"
-              ></input>
-              <label></label>
+        <div className={style.content}>
+          <h3 className={style.title_mobile}>Transferência</h3>
+          <form className={style.form}>
+            <div className={style.col_1}>
+              <div className={style.container_input_label}>
+                <input
+                  type="number"
+                  pattern="[0-9]{3}"
+                  maxLength={3}
+                  minLength={3}
+                  required
+                  autoComplete="off"
+                  placeholder="Cod do banco"
+                ></input>
+                <label></label>
+              </div>
+              <div className={style.container_input_label}>
+                <input
+                  type="number"
+                  pattern="[0-9]*"
+                  autoComplete="0ff"
+                  required
+                  placeholder="agência"
+                ></input>
+                <label></label>
+              </div>
             </div>
-            <div className={style.container_input_label}>
-              <input
-                type="number"
-                pattern="[0-9]*"
-                autoComplete="0ff"
-                required
-                placeholder="agência"
-              ></input>
-              <label></label>
+            <div className={style.col_2}>
+              <div className={style.container_input_label}>
+                <input
+                  type="number"
+                  pattern="[0-9]*"
+                  autoComplete="off"
+                  required
+                  placeholder="Nº conta"
+                ></input>
+                <label></label>
+              </div>
+              <div className={style.container_input_label}>
+                <input
+                  type="number"
+                  pattern="[0-9.,]+"
+                  autoComplete="off"
+                  required
+                  placeholder="valor"
+                ></input>
+                <label></label>
+              </div>
             </div>
-          </div>
-
-          <div className={style.col_2}>
-            <div className={style.container_input_label}>
-              <input
-                type="number"
-                pattern="[0-9]*"
-                autoComplete="off"
-                required
-                placeholder="Nº conta"
-              ></input>
-              <label></label>
+            <div className={style.col_3}>
+            <div className={style.container_btn}>
+              {/* <button className={style.btn} type="submit">Próximo</button> */}
+              <MostrarModalTransferencia />
             </div>
-            <div className={style.container_input_label}>
-              <input
-                type="number"
-                pattern="[0-9.,]+"
-                autoComplete="off"
-                required
-                placeholder="valor"
-              ></input>
-              <label></label>
+      
+            <div className={style.container_btn}>
+              {/* <button className={style.btn} type="submit">Próximo</button> */}
+              <MostrarModalTransferencia />
             </div>
-          </div>
-
-          <div className={style.col_3}>
-
-
-
-          <div className={style.container_btn}>
-            {/* <button className={style.btn} type="submit">Próximo</button> */}
-            <MostrarModalTransferencia />
-          </div>
-          
-          <div className={style.container_btn}>
-            {/* <button className={style.btn} type="submit">Próximo</button> */}
-            <MostrarModalTransferencia />
-          </div>
-
-          </div>
-        </form>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
